82f76f3470ffd065a61645831ef9b2c217582394
talk-chain <talkchain@proton.me>
2025-11-15 16:46:49 +0900

    chore(router): opt-in to React Router v7_startTransition future flag to silence warning and wrap state updates in startTransition

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,12 @@ import { RouterProvider } from 'react-router-dom'
 import router from './app/routes'
 
 export default function App() {
-  return <RouterProvider router={router} />
+  return (
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
+  )
 }
